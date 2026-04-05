1162fd0687a4ebb8f0bd47dbaa8045942b7c74b0
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2026-04-05 22:10:18 +0100

    Add comment to firstNameCustomer field
    Added comment to firstNameCustomer field for clarity.

--- my_progect/my_project/bartermonster/models.py
+++ my_progect/my_project/bartermonster/models.py
@@ -2,5 +2,5 @@
 from django.db import models
 
 class Customers(models.Model):
-firstNameCustomer = models.CharField(max_length=255)
+firstNameCustomer = models.CharField(max_length=255) #CharField=text field
 lastNameCustomer = models.CharField(max_length=255)
