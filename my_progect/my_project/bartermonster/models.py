517cb5970ca5672b5ab57cf6a33889f337174b0a
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2026-04-08 22:06:55 +0100

    Add migration command comment in models.py
    Added a comment for running migrations in the models file.

--- my_progect/my_project/bartermonster/models.py
+++ my_progect/my_project/bartermonster/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Customers(models.Model):
 firstNameCustomer = models.CharField(max_length=255) #CharField=text field
 lastNameCustomer = models.CharField(max_length=255)
+#run this command: python manage.py makemigrations members
