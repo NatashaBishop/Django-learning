35a74f500a56eb07ebee79396390bd49cceb25af
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2026-04-09 17:01:58 +0100

    migration results

--- my_progect/my_project/bartermonster/models.py
+++ my_progect/my_project/bartermonster/models.py
@@ -5,3 +5,10 @@ class Customers(models.Model):
 firstNameCustomer = models.CharField(max_length=255) #CharField=text field
 lastNameCustomer = models.CharField(max_length=255)
 #run this command: python manage.py makemigrations members
+#with output:
+'''Migrations for 'members':
+  members\migrations\0001_initial.py
+    - Create model Member
+
+(myworld) C:\Users\Your Name\myworld\my_tennis_club> '''
+
