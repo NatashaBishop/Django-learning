18a5b3cb26df78845a8ceed40cdf2534b033c852
Natasha Bishop <51851566+NatashaBishop@users.noreply.github.com>
2026-05-31 15:01:12 +0100

    add examples for views.py
    Added comments to illustrate good and bad examples of implementing business logic in the ListingView class.

--- Django_REST _framework/DRFflowUsecase/views.py
+++ Django_REST _framework/DRFflowUsecase/views.py
@@ -4,3 +4,12 @@
                 walks = Walk.objects.all()
                 serializer = WalkSerializer(walks, many=True)
                 return Response(serializer.data)
+# bad example - do not emplement - 200 lines of business logic:
+class ListingView(APIView):
+    def post(self, request):
+            
+# good example - emplement this way:
+class ListingView(APIView):
+    def post(self, request):
+        listing = create_listing(request.data)
+        return Response(...)
